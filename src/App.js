46ed0011172d127d1b50a5f97a621f6a6a79a49a
Sindhupothaganti <sindhupothaganti17.com>
2021-11-18 13:07:27 +0530

    context for passing different data btw diff components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import DataFetching from './components/hooks/DataFetching';
+import DataFetching from './components/useeffecthook/DataFetching';
+import ComponentA from './components/contexthook/ComponentA';
+import React from 'react';
+
+
+export const UserContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-     <DataFetching/>
+      <UserContext.Provider value = {'Sindhu Pothaganti'} >
+        <ComponentA/>
+     </UserContext.Provider>
     </div>
   );
 }
-
 export default App;
