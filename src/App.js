fe3642ffb878866959b13b7641e89ba06d65859c
Sindhupothaganti <sindhupothaganti17.com>
2021-11-22 16:30:41 +0530

    data fetching useReducer hook

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import CompA from './components/contextNreducer/CompA';
 import CompB from './components/contextNreducer/CompB';
 import CompC from './components/contextNreducer/CompC';
 import DataFectching from './components/useReducercases/DataFectching';
+import DataFetchingTwo from './components/useReducercases/DataFetchingTwo';
 
 export const CountContext = React.createContext()
 const initialState = 0
-
 const reducer = (state, action) => {
   switch (action) {
     case 'increment': return state + 1
@@ -19,15 +19,13 @@ const reducer = (state, action) => {
     default: return state
   }
 }
-
 function App() {
   return (
 
     <div className="App">
-      <DataFectching />
+      <DataFetchingTwo />
     </div>
 
   );
 }
-
-export default App;
+export default App
