cea3910bc83f0f259bf4724fa682d0b81a238bd1
Sindhupothaganti <sindhupothaganti17.com>
2021-11-22 10:10:00 +0530

    use context hook to pass the data around

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import DataFetching from './components/useeffecthook/DataFetching';
 import ComponentA from './components/contexthook/ComponentA';
 import React from 'react';
 
 
-export const UserContext = React.createContext()
+export const UserContext = React.createContext()     //exporting a context
+export const BatchContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value = {'Sindhu Pothaganti'} >
-        <ComponentA/>
-     </UserContext.Provider>
+      <UserContext.Provider value={'Sindhu Pothaganti'} >
+        <BatchContext.Provider value={'dxc batch 2'}>
+          <ComponentA />
+        </BatchContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
+
 export default App;
