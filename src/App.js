ed986222a326b49144778fdc46644dc5908ddb15
Sindhupothaganti <sindhupothaganti17.com>
2021-11-24 10:35:09 +0530

    life b4 higher order components -- part2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ClickCounter from './components/higherorderComponents/ClickCounter';
+import HoverCounter from './components/higherorderComponents/HoverCounter';
+
 function App() {
   return (
-
     <div className="App">
      <ClickCounter/>
+     <HoverCounter/>
     </div>
 
   );
